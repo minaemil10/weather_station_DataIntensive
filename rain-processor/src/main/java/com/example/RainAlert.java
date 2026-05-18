7f091cddfb69285804554e3b65cf3abba16e8ddb
Mina Emile <minaemile3@gmail.com>
2026-05-18 13:34:13 +0300

    working processor el7

---
+++ rain-processor/src/main/java/com/example/RainAlert.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public class RainAlert {
+    public Long station_id;
+    public String alert;
+}
---
+++ rain-processor/src/main/java/com/example/Weather.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Weather {
+    public Integer humidity;
+    public Integer temperature;
+    public Integer wind_speed;
+}
